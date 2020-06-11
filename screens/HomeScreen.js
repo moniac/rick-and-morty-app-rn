0399059dcecfe6937d93b4659c222dea13a82e8d
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-06-11 22:42:18 +0200

    add native paper

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,12 +3,13 @@ import React, { useEffect, useState } from 'react';
 import {
 	Image,
 	StyleSheet,
-	TextInput,
 	View,
 	FlatList,
-	Button,
+	// Button
 } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import { getCharacter } from '../hooks/useApi';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
 export default function HomeScreen(props) {
 	const [characters, setCharacters] = useState([]);
@@ -16,14 +17,15 @@ export default function HomeScreen(props) {
 	const [pageLimit, setPageLimit] = useState(undefined);
 	const { navigation } = props;
 
-	const [search, setSearch] = useState(undefined);
+	const [search, setSearch] = useState('');
 
 	useEffect(() => {
 		console.log(search);
-		if (!search) {
-			setCharacters([]);
+
+		if (nextPage > 1) {
 			setNextPage(1);
 		}
+
 		async function getCharacterBySearch() {
 			const response = await getCharacter({ name: search });
 			setCharacters(response.results);
@@ -75,41 +77,49 @@ export default function HomeScreen(props) {
 				numColumns={1}
 				data={characters}
 				ListHeaderComponent={
-					<TextInput
-						onChangeText={(event) => setSearch(event.trim())}
-					/>
+					<View style={{ width: '100%' }}>
+						<TextInput
+							style={{ width: 300, flex: 1 }}
+							onChangeText={(event) => setSearch(event.trim())}
+						/>
+					</View>
 				}
+				ListHeaderComponentStyle={{ width: '100%' }}
 				contentContainerStyle={{
 					justifyContent: 'center',
 					alignItems: 'center',
 					alignSelf: 'center',
+					width: '100%',
 				}}
+				style={{ width: '100%' }}
 				renderItem={({ item }) => (
-					<View>
+					<Card>
 						<View>
-							<Image
+							<Card.Cover
 								style={{
 									width: 300,
 									height: 300,
-									marginTop: 12,
+									paddingTop: 12,
 								}}
-								resizeMode={'cover'}
 								source={{ uri: item.image }}
 							/>
 						</View>
-
-						<View>
-							<Button
-								title={item.name}
-								onPress={() =>
-									navigation.navigate('CharacterDetail', {
-										characterId: item.id,
-										title: item.name,
-									})
-								}
-							/>
-						</View>
-					</View>
+						<Card.Content>
+							<Card.Title title={item.name} />
+							<Card.Actions>
+								<Button
+									onPress={() =>
+										navigation.navigate('CharacterDetail', {
+											characterId: item.id,
+											title: item.name,
+										})
+									}
+								>
+									Visit
+								</Button>
+							</Card.Actions>
+						</Card.Content>
+					</Card>
 				)}
 				keyExtractor={(char) => `${char.id} - ${char.name}`}
 				onEndReached={() => {
@@ -138,5 +148,6 @@ HomeScreen.navigationOptions = {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		width: '100%',
 	},
 });
